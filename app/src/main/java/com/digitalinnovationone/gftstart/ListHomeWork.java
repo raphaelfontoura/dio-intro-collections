0e5b5d172c02f51a612728645560a3c2d176119d
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-06-05 11:42:53 -0300

    Add input read

--- app/src/main/java/com/digitalinnovationone/gftstart/ListHomeWork.java
+++ app/src/main/java/com/digitalinnovationone/gftstart/ListHomeWork.java
@@ -1,5 +1,8 @@
 package com.digitalinnovationone.gftstart;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,16 +16,21 @@ public class ListHomeWork {
             "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"));
 
     public static void main(String[] args) {
-        List<Double> temperatureList = new ArrayList<>() {
-            {
-                add(21d);
-                add(25d);
-                add(28d);
-                add(24.3);
-                add(27.8);
-                add(31d);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
+        List<Double> temperatureList = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Digite a temperatura do mês: ");
+            try {
+                temperatureList.add(Double.parseDouble(in.readLine()));
+            } catch (NumberFormatException e) {
+                System.out.println("Digite um valor válido. Ex.: 24.5");
+                --i;
+            } catch (IOException e) {
+                System.out.println("Falha na leitura do terminal");
+                System.out.println(e.getMessage());
             }
-        };
+        }
 
         Double mean = calcMeanDouble(temperatureList, 6);
 
