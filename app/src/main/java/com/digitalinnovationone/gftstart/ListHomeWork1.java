8b2f2c9682fec12ec0856ed1e3a4cb8f352cc81c
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-06-05 19:48:03 -0300

    Change BufferedReader to Scanner and refactor code

--- app/src/main/java/com/digitalinnovationone/gftstart/ListHomeWork1.java
+++ app/src/main/java/com/digitalinnovationone/gftstart/ListHomeWork1.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * ListHomeWork
@@ -16,23 +18,15 @@ public class ListHomeWork1 {
             "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"));
 
     public static void main(String[] args) {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        
+        Scanner input = new Scanner(System.in);
+
         List<Double> temperatureList = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            System.out.print("Digite a temperatura do mês: ");
-            try {
-                temperatureList.add(Double.parseDouble(in.readLine()));
-            } catch (NumberFormatException e) {
-                System.out.println("Digite um valor válido. Ex.: 24.5");
-                --i;
-            } catch (IOException e) {
-                System.out.println("Falha na leitura do terminal");
-                System.out.println(e.getMessage());
-            }
+        for (int i = 1; i <= 6; i++) {
+            System.out.printf("Digite a temperatura do mês %d: ", i);
+            temperatureList.add(input.nextDouble());
         }
 
-        Double mean = calcMeanDouble(temperatureList, 6);
+        Double mean = sumList(temperatureList) / temperatureList.size();
 
         System.out.printf("A temperatura média do semestre foi: %.2f. \n", mean);
         // Collections.sort(temperatureList, Collections.reverseOrder());
@@ -57,13 +51,12 @@ public class ListHomeWork1 {
         return resultList;
     }
 
-    private static Double calcMeanDouble(List<Double> temperatureList, Integer count) {
+    private static Double sumList(List<Double> temperatureList) {
         Double sum = 0d;
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < temperatureList.size(); i++) {
             sum += temperatureList.get(i);
         }
-        Double mean = sum / count;
-        return mean;
+        return sum;
     }
 
 }
